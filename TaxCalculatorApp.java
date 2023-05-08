b7715dc68055b513c8f6ccc1414b6ee77145279e
Ratul Saha <68448205+ratulsaha-dev@users.noreply.github.com>
2023-05-08 20:33:38 +0530

    Create TaxCalculatorApp.java

---
+++ TaxCalculatorApp.java
@@ -0,0 +1,18 @@
+package mainCal;
+import java.util.*;
+
+public class TaxCalculatorApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		TaxCalculator t = new TaxCalculator();
+		System.out.println(" Enter the amount");
+		double income = scan.nextDouble();
+		scan.close();
+		double tax = t.calculateTax(income);
+        System.out.println("Tax: " + tax);
+		
+	}
+
+}
