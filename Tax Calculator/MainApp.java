07e138277368c7d905b1bf4fb182b83ba32535ad
Ratul Saha <68448205+ratulsaha-dev@users.noreply.github.com>
2023-05-08 22:41:44 +0530

    Update and rename TaxCalculatorApp.java to MainApp.java

--- Tax Calculator/MainApp.java
+++ Tax Calculator/MainApp.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class TaxCalculatorApp {
+public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
